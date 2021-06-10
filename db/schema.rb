1bc96cff511dd06bb11979f37c5faf23fbef14aa
docty89 <docty89@gmail.com>
2021-06-10 15:03:13 +0000

    フォロー機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_06_025928) do
+ActiveRecord::Schema.define(version: 2021_06_10_052651) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -53,6 +53,13 @@ ActiveRecord::Schema.define(version: 2021_06_06_025928) do
     t.string "name"
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

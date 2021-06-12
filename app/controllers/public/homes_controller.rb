901bd52cfa61b1fa75d83bdce86b2d542439ef90
docty89 <docty89@gmail.com>
2021-06-12 13:23:47 +0000

    top追記

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,6 @@ class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
     # limit=>指定した件数取得,order=>並び替え,desc=>降順
-    @posts = Post.limit(4).order(create_at: :desc)
+    @posts = Post.limit(4).order(created_at: :desc)
   end
 end

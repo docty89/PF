392ca911507761e137aa0f91b477c0b3ebe6fb20
docty89 <docty89@gmail.com>
2021-08-11 15:03:59 +0000

    投稿の価格を0以上の正の数のみ受け付けるようにバリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,9 @@ class Post < ApplicationRecord
 
   validates :name, :price, :image, :body, :storage, :expired, :prefecture_code, presence: true
 
+  # 0以上の正の数のみ(greater_than_or_equal_to: 0)、整数のみ(only_integer: true)
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
   # 引数で渡されたユーザidがFavoritesテーブル内に存在(exists?)していればtrue,存在していなければfalseを返す。
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
